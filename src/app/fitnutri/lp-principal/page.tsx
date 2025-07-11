12461a8758cec7f9ee050770528f8365415d2d5a
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-07-10 22:51:29 -0300

    Update page.tsx

--- src/app/fitnutri/lp-principal/page.tsx
+++ src/app/fitnutri/lp-principal/page.tsx
@@ -1,1 +1,2 @@
+import LandingPage from '@/components/LandingPage'; import { LandingPageData } from '@/types/lp-config'; import lpData from './lp.json';  const config = lpData as LandingPageData;  export default function FitNutriPage() {   return <LandingPage data={config} />; }
 
